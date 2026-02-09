05f425bca01fc608cdf955dad60ba2396ad36d1e
gela <bichashvili.gela@softgen.ge>
2026-02-09 20:15:30 +0400

    ADD CI/CD

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-change-this-in-productio
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list(
-    'ALLOWED_HOSTS',
-    default=['localhost', '127.0.0.1', '192.168.80.230', '*']
-)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
 # Application definition
 INSTALLED_APPS = [
