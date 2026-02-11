beb717bcfe7317259efb3411af832aa7796211ad
gela <bichashvili.gela@softgen.ge>
2026-02-11 15:16:57 +0400

    ADD CI/CD

--- config/settings.py
+++ config/settings.py
@@ -378,7 +378,7 @@ LOGGING = {
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
